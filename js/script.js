ee5809e6625abd83b537df02669c9c85ad47286f
Mayank Soni <sonimayank151@gmail.com>
2024-04-30 16:54:45 +0530

    Popular TV-Shows

--- js/script.js
+++ js/script.js
@@ -16,15 +16,17 @@ async function fetchAPIData(endpoint) {
     const API_KEY = '4d4dd536f4794961a5e43303ce129155'
     const API_URL = 'https://api.themoviedb.org/3/';
   
+    showSpinner()
     const response = await fetch(
       `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
     );
   
     const data = await response.json();
+
+    hideSpinner()
   
     return data;
   }
-
 
 async function displayPopularMovies(){
     const {results} = await fetchAPIData("movie/popular")
@@ -44,7 +46,7 @@ async function displayPopularMovies(){
           : `<img
           src = "../images/no-image.jpg"
           class = "card-img-top"
-          alt = "${movie.title}"
+          alt = "s${movie.title}"
           />`
         }
         </a>
@@ -59,6 +61,47 @@ async function displayPopularMovies(){
     })
 }
 
+function showSpinner() {
+    document.querySelector(".spinner").classList.add("show")
+}
+
+function hideSpinner() {
+    document.querySelector(".spinner").classList.remove("show")
+}
+
+async function displayPopularTVShows() {
+    const {results} = await fetchAPIData("tv/popular")
+
+    results.forEach((show) => {
+        const div = document.createElement("div");
+        div.classList.add("card")
+        div.innerHTML = `
+        <a href="movie-details.html?id=${show.id}">
+        ${
+            show.poster_path
+            ?`<img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+          : `<img
+          src = "../images/no-image.jpg"
+          class = "card-img-top"
+          alt = "s${show.name}"
+          />`
+        }
+        </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Release: ${show.first_air_date}</small>
+          </p>
+        </div>
+        `
+        document.querySelector("#popular-shows").appendChild(div)
+    })
+}
+
 // Init App
 function init() {
     switch(navigator.currentPage) {
@@ -67,7 +110,7 @@ function init() {
             displayPopularMovies()
             break
         case "/shows.html":
-            console.log("TV Shows")
+            displayPopularTVShows()
             break
         case "/tv-details.html":
             console.log("TV Details")
