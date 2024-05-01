b75d908597bb42311dd667bac391dcbff59f6345
Mayank Soni <sonimayank151@gmail.com>
2024-05-01 15:36:11 +0530

    update functionalities

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ async function displayPopularTVShows() {
         const div = document.createElement("div");
         div.classList.add("card")
         div.innerHTML = `
-        <a href="movie-details.html?id=${show.id}">
+        <a href="tv-details.html?id=${show.id}">
         ${
             show.poster_path
             ?`<img
@@ -102,11 +102,102 @@ async function displayPopularTVShows() {
     })
 }
 
+// Displaying background/backdrop image
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDiv = document.createElement("div")
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`
+    overlayDiv.style.backgroundSize = "cover"
+    overlayDiv.style.backgroundPosition = "center"
+    overlayDiv.style.backgroundRepeat = "no-repeat"
+    overlayDiv.style.hight = "100vh"
+    overlayDiv.style.width = "100vh"
+
+    if (type === "movie"){
+        document.querySelector("#movie-details").appendChild(overlayDiv)
+    }
+    else{
+        document.querySelector("#show-details").appendChild(overlayDiv)
+    }
+}
+
+// Displaying Movie Detail for selected Movie
+async function displayMovieDetails() {
+    const movieId = window.location.search.split("=")[1]
+    
+    const movie = await fetchAPIData(`movie/${movieId}`)
+
+    displayBackgroundImage("movie", movie.backdrop_path)
+
+    const div = document.createElement("div")
+
+    div.innerHTML = `
+    <div class = "details-top">
+    <div>
+    ${
+        movie.poster_path
+        ?`<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`
+      : `<img
+      src = "../images/no-image.jpg"
+      class = "card-img-top"
+      alt = "s${movie.title}"
+      />`
+    }
+    </div>
+    <div>
+    <h2>${movie.title}</h2>
+    <p>
+        <i class="fas fa-star text-primary"></i>
+         ${movie.vote_average.toFixed(1)}/ 10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    <p>
+        ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+        ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+        movie.homepage
+    }" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+</div>
+<div class="details-bottom">
+    <h2>Movie Info</h2>
+    <ul>
+    <li><span class="text-secondary">Budget:</span> $${addcommasToNumber(movie.budget)}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addcommasToNumber(movie.revenue)}</li>
+    <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${movie.production_companies
+    .map((company) => `<span>${company.name}</span>`)
+    .join(", ")}
+    </div>
+</div>
+    `
+
+    document.querySelector("#movie-details").appendChild(div)
+}
+
+
+// Displaying TV-Shows detail
+
+function addcommasToNumber(number){
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
 // Init App
 function init() {
     switch(navigator.currentPage) {
+        case "/index.html":
         case "/":
-        case "/index":
             displayPopularMovies()
             break
         case "/shows.html":
@@ -116,7 +207,7 @@ function init() {
             console.log("TV Details")
             break
         case "/movie-details.html":
-            console.log("Movies Details")
+            displayMovieDetails()
             break
         case "/search.html":
             console.log("Search page")
